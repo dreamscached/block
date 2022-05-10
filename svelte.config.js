b5ba6896fa721ab977c899b169f63cd64c6a56d2
dreamscached <dreamscache.d@gmail.com>
2022-05-10 11:40:58 +0200

    Fix config

--- svelte.config.js
+++ svelte.config.js
@@ -3,24 +3,9 @@ import preprocess from 'svelte-preprocess'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
 	preprocess: preprocess(),
-
 	kit: {
 		adapter: adapter()
-		// adapter: adapter({
-		//     pages: 'build',
-		//     assets: 'build',
-		//     fallback: null
-		// }),
-		// paths: {
-		//     base: '/block'
-		// },
-		// appDir: 'internal',
-		// prerender: {
-		//     default: true
-		// }
 	}
 }
 
