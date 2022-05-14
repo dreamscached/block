b9b54f2f556f9306f0a28777f1d8ede1d416e06d
dreamscached <dreamscache.d@gmail.com>
2022-05-14 23:09:51 +0200

    Update bottom text

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -56,14 +56,14 @@
 
 <div class='text-center text-gray-400 select-none fixed w-full bottom-10'>
 	<div class='w-2/3 m-auto'>
-		<span class='text-md md:text-xl'>
-			<a class='text-green-400' href='https://github.com/dreamscached/block' target='_blank'>block</a>
+		<span class='text-md md:text-xl font-light'>
+			<a class='text-green-400 font-normal' href='https://github.com/dreamscached/block' target='_blank'>block</a>
 			is made by
-			<a class='text-blue-400' href='https://github.com/dreamscached' target='_blank'>@dreamscached</a>
+			<a class='text-blue-400 font-normal' href='https://github.com/dreamscached' target='_blank'>@dreamscached</a>
 			using
-			<a class='text-orange-400' href='https://svelte.dev/' target='_blank'>svelte</a>
+			<a class='text-orange-400 font-normal' href='https://svelte.dev/' target='_blank'>svelte</a>
 			and lots of
-			<svg xmlns='http://www.w3.org/2000/svg' class='fill-red-400 h-5 w-5 mb-1 inline' viewBox='0 0 20 20'
+			<svg xmlns='http://www.w3.org/2000/svg' class='fill-red-400 h-6 w-6 mb-1.5 inline' viewBox='0 0 20 20'
 				 fill='currentColor'>
 			  <path fill-rule='evenodd' clip-rule='evenodd'
 					d='M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z'
